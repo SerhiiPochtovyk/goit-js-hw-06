fc095c54291efcf2dc7d623d6f804b09083e5dba
SerhiiPochtovyk <sergey.po4tovik@gmail.com>
2023-09-20 09:24:38 +0300

    fix task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -7,10 +7,21 @@ const ingredients = [
   "Condiments",
 ];
 
-const ul = document.getElementById('ingredients'); // Знаходимо список ul по його id
-ingredients.forEach(ingredient => { // Проходимо по кожному інгредієнту в масиві
-  const li = document.createElement('li'); // Створюємо новий елемент li
-  li.innerText = ingredient; // Встановлюємо текстовий вміст елементу li
-  li.classList.add('item'); // Додаємо клас "item" до елементу li
-  ul.appendChild(li); // Додаємо елемент li до списку ul
+// const ul = document.getElementById('ingredients'); // Знаходимо список ul по його id
+// ingredients.forEach(ingredient => { // Проходимо по кожному інгредієнту в масиві
+//   const li = document.createElement('li'); // Створюємо новий елемент li
+//   li.innerText = ingredient; // Встановлюємо текстовий вміст елементу li
+//   li.classList.add('item'); // Додаємо клас "item" до елементу li
+//   ul.appendChild(li); // Додаємо елемент li до списку ul
+// });
+
+const ulIngredients = document.getElementById("ingredients");
+
+const listIngredients = ingredients.map((ingredient) => {
+  const listIngredient = document.createElement("li");
+  listIngredient.textContent = ingredient;
+  listIngredient.classList.add("item");
+  return listIngredient;
 });
+
+ulIngredients.append(...listIngredients);
